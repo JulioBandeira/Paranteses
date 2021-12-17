7bb5c3e6145367911b492fd27b7d82ecfee1bd55
Julio César da Silva Bandeira <info.bandeira@gmail.com>
2021-12-17 11:29:25 -0300

    continue algorito

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -7,14 +7,18 @@ namespace ConsoleApp8
         static void Main(string[] args)
         {
             var parenteses = new string[] { "{}", "[]", "()" };
-            //var parenteses_input = "{[()]}";
+            var parenteses_input = "{[()]}";
             //var parenteses_input = "{}[()]";
-            var parenteses_input = "{}[]()";
+            //var parenteses_input = "{}[]()";
+            //var parenteses_input = "{}[]";
             //var parenteses_input = "{}";
             char[] parenteses_array = parenteses_input.ToCharArray();
             var n = parenteses_array.Length;
             var array_open = new char[n];
             var array_close = new char[n];
+            var array_close_open = new string[n];
+            var temp_array = array_close_open;
+            var cont_array_true = 0;
 
             for (int i = 0; i < n / 2; i++)
                 array_open[i] = parenteses_array[i];
@@ -22,7 +26,8 @@ namespace ConsoleApp8
             var cont = 0;
             for (int i = n; i > 0; i--)
             {
-                if ((i - 1) >= 3)
+              
+                if ((i - 1) >= n/2)
                 {
                     array_close[cont] = parenteses_array[i - 1];
                     cont = cont + 1;
@@ -31,14 +36,22 @@ namespace ConsoleApp8
 
             string blockeds = "";
 
-            for (int i = 0; i < parenteses.Length; i++)
-            {
+            for (int i = 0; i < array_open.Length; i++) {
+
                 blockeds = $"{array_open[i]}{array_close[i]}";
 
-                if (blockeds == parenteses[i])
+                for (int j = 0; j < parenteses.Length; j++)
+                {
+                    if (blockeds == parenteses[j]) {
 
-                    Console.WriteLine("true");
+                        cont_array_true = cont_array_true + 1;
+                        Console.WriteLine(cont_array_true);
+                    }
+
+                }
             }
+
+            Console.WriteLine((n/2) == cont_array_true ? true : false);
 
         }
     }
