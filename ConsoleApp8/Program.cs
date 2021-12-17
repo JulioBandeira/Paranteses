dccdde6f1adf353f9e61cef3ce2f3e30864a891b
Julio César da Silva Bandeira <info.bandeira@gmail.com>
2021-12-17 15:04:47 -0300

    continue algoritmo

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -7,6 +7,50 @@ namespace ConsoleApp8
         static void Main(string[] args)
         {
             var parenteses = new string[] { "{}", "[]", "()" };
+            //var parenteses_input = "{[()]}";
+            //var parenteses_input = "{}[()]";
+            //var parenteses_input = "{}[]()";
+            //var parenteses_input = "{}[]";
+            var parenteses_input = "{}";
+            char[] parenteses_array = parenteses_input.ToCharArray();
+            var n = parenteses_array.Length / 2;
+            var array_blocked = new string[n];
+            var cont_array_true = 0;
+
+            var cont = 0;
+            for (int i = 0; i < parenteses_array.Length; i++)
+            {
+                if ((i % 2) == 0)
+                {
+
+                    //Console.WriteLine($"{(i % 2) == 0}");
+                    //Console.WriteLine($"{parenteses_array[i]}{parenteses_array[i + 1]}");
+                    //Console.WriteLine($"{parenteses_array[i]}{parenteses_array[i + 1]}");
+                    array_blocked[cont] = $"{parenteses_array[i]}{parenteses_array[i + 1]}";
+                    cont = cont + 1;
+                }
+            }
+
+
+            for (int i = 0; i < array_blocked.Length; i++)
+            {
+                for (int j = 0; j < parenteses.Length; j++)
+                {
+                    if (array_blocked[i] == parenteses[j]) { 
+                    
+                        cont_array_true = cont_array_true + 1;
+                    }
+
+                    else Console.WriteLine(false);
+                }
+            }
+
+        }
+
+        public static void OpenCloseArray()
+        {
+
+            var parenteses = new string[] { "{}", "[]", "()" };
             var parenteses_input = "{[()]}";
             //var parenteses_input = "{}[()]";
             //var parenteses_input = "{}[]()";
@@ -14,6 +58,8 @@ namespace ConsoleApp8
             //var parenteses_input = "{}";
             char[] parenteses_array = parenteses_input.ToCharArray();
             var n = parenteses_array.Length;
+
+
             var array_open = new char[n];
             var array_close = new char[n];
             var array_close_open = new string[n];
@@ -26,8 +72,8 @@ namespace ConsoleApp8
             var cont = 0;
             for (int i = n; i > 0; i--)
             {
-              
-                if ((i - 1) >= n/2)
+
+                if ((i - 1) >= n / 2)
                 {
                     array_close[cont] = parenteses_array[i - 1];
                     cont = cont + 1;
@@ -36,13 +82,15 @@ namespace ConsoleApp8
 
             string blockeds = "";
 
-            for (int i = 0; i < array_open.Length; i++) {
+            for (int i = 0; i < array_open.Length; i++)
+            {
 
                 blockeds = $"{array_open[i]}{array_close[i]}";
 
                 for (int j = 0; j < parenteses.Length; j++)
                 {
-                    if (blockeds == parenteses[j]) {
+                    if (blockeds == parenteses[j])
+                    {
 
                         cont_array_true = cont_array_true + 1;
                         Console.WriteLine(cont_array_true);
@@ -51,7 +99,7 @@ namespace ConsoleApp8
                 }
             }
 
-            Console.WriteLine((n/2) == cont_array_true ? true : false);
+            Console.WriteLine((n / 2) == cont_array_true ? true : false);
 
         }
     }
