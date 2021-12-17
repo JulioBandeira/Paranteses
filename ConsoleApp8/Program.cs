1a6180a662dadaa6aa2be85155c279fbeeb5bea1
Julio César da Silva Bandeira <info.bandeira@gmail.com>
2021-12-17 06:33:30 -0400

    criacao da rotina para concater os parenteses

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -6,29 +6,28 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            var parenteses = "{[()]}";
+            var parenteses_input = "{[()]}";
+            char[] parenteses_array = parenteses_input.ToCharArray();
+            var n = parenteses_array.Length;
+            var array_open = new char[n];
+            var array_close = new char[n];
 
-            char[] parenteses_array = parenteses.ToCharArray();
-
-            for (int i = 0; i < parenteses_array.Length / 2; i++) { 
-            
-                Console.WriteLine(parenteses_array[i]);
-                Console.WriteLine(i);
-            }          
-
-            Console.WriteLine("__________________________");
-
-            for (int i = parenteses_array.Length; i > 0; i--)
+            for (int i = 0; i < n / 2; i++) 
+                array_open[i] = parenteses_array[i];
+                                  
+            var cont = 0;
+            for (int i = n; i > 0; i--)
             {
                 if ((i - 1) >= 3)
                 {
-                    Console.WriteLine(parenteses_array[i - 1]);
-                    Console.WriteLine(i);
-                }
-                    
+                    array_close[cont] = parenteses_array[i - 1];                  
+                    cont = cont + 1;
+                }  
             }
-            
 
+            for (int i = 0; i < array_open.Length; i++)          
+                Console.WriteLine($"{array_open[i]}{array_close[i]}");
+                 
         }
     }
 }
