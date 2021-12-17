607d768997de794fbecfb3bd5b3bbc46e73ff8e9
Julio César da Silva Bandeira <info.bandeira@gmail.com>
2021-12-17 10:41:41 -0300

    condicional para verificar array

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -6,28 +6,40 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            var parenteses_input = "{[()]}";
+            var parenteses = new string[] { "{}", "[]", "()" };
+            //var parenteses_input = "{[()]}";
+            //var parenteses_input = "{}[()]";
+            var parenteses_input = "{}[]()";
+            //var parenteses_input = "{}";
             char[] parenteses_array = parenteses_input.ToCharArray();
             var n = parenteses_array.Length;
             var array_open = new char[n];
             var array_close = new char[n];
 
-            for (int i = 0; i < n / 2; i++) 
+            for (int i = 0; i < n / 2; i++)
                 array_open[i] = parenteses_array[i];
-                                  
+
             var cont = 0;
             for (int i = n; i > 0; i--)
             {
                 if ((i - 1) >= 3)
                 {
-                    array_close[cont] = parenteses_array[i - 1];                  
+                    array_close[cont] = parenteses_array[i - 1];
                     cont = cont + 1;
-                }  
+                }
             }
 
-            for (int i = 0; i < array_open.Length; i++)          
-                Console.WriteLine($"{array_open[i]}{array_close[i]}");
-                 
+            string blockeds = "";
+
+            for (int i = 0; i < parenteses.Length; i++)
+            {
+                blockeds = $"{array_open[i]}{array_close[i]}";
+
+                if (blockeds == parenteses[i])
+
+                    Console.WriteLine("true");
+            }
+
         }
     }
 }
